4b45fb0126e6f5ef5db4fc9830285ba0fd20fa01
Surya Narayan Bera <suryanarayan.bera@cognizant.com>
2026-07-16 18:13:37 -0400

    test3

--- src/main/java/com/example/demo/GreetingService.java
+++ src/main/java/com/example/demo/GreetingService.java
@@ -14,6 +14,8 @@ public class GreetingService {
 	public void logName(String name) {
 		log.info("User entered name = {}", name);
 		System.out.println("User entered name = " + name);
+		System.out.println("User entered name = " + name);
+		//TODO: Add more logic here if needed
 		//TODO: Add more logic here if needed
 		//TODO: Add more logic here if needed
 	}
